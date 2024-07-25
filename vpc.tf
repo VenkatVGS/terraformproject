ab077ed4c9333605bd1e1dc122d729c5e14b063b
VenkatVGS <venkat.tendulkar@gmail.com>
2024-07-25 23:24:16 +0530

    2nd commit

--- vpc.tf
+++ vpc.tf
@@ -21,7 +21,7 @@ resource "aws_vpc" "myvpc" {
 }
 
 # Create Pub Subnet
-resource "aws_subnet" "pub-subnet" {
+resource "aws_subnet" "pub-subnets" {
   vpc_id     = aws_vpc.myvpc.id
   cidr_block = "10.0.1.0/24"
   availability_zone = "ap-south-1a"
